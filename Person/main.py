e16878731bb56f3d4db9dd45543651a8f0ccaf03
FanniBelics <fanni.belics@gmail.com>
2025-10-04 10:12:37 +0200

    Separating person.py from main.py

--- Person/main.py
+++ Person/main.py
@@ -1,34 +1,4 @@
-
-class Person:
-    id: str
-    name: str
-    age: int
-    smoking: bool
-
-    def __init__(self, id, name, age, smoking):
-        self.id = id
-        self.name = name
-        self.age = age
-        self.smoking = smoking
-
-    def __str__(self):
-        return '[id = {0}] : name = {1}, age = {2}, smoking = {3}'.format(self.id, self.name, self.age, self.smoking)
-
-    def __repr__(self):
-        return "{id = {0} name = {1}, age = {2}, smoking = {3}}.".format(self.id, self.name, self.age, self.smoking)
-
-    def __eq__(self, other):
-        return self.id == other.id
-
-    def __lt__(self, other):
-        return self.id < other.id
-
-    def __le__(self, other):
-        return self.id <= other.id
-
-    def __hash__(self):
-        return hash(self.id)
-
+from person import Person
 
 def main():
     print("Hello World")
@@ -41,7 +11,9 @@ def main():
     print(p1 == p2)
     print(p1 == p3)
 
-    print(p1 > p2)
+    print(p1.__gt__(p2))
+    print(p1>p2)
+    print(p1>p3)
 
     print(p1.__hash__())
 
